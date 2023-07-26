fac9823a70d9f80247879e25b7b8ecf9b18e859c
JJ PC <hellojjlin@gmail.com>
2023-07-26 16:40:02 -0400

    Objects

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// --Variables--
 // Cannot be a reserved keyword
 // Should be meaningful
 // Cannot start with a number
@@ -7,6 +8,7 @@
 let firstName = 'JJ';
 let lastName = 'Lin';
 
+// let vs const
 // const means constant, the variable cannot be changed
 // if the variable needs to be changed use let instead
 
@@ -18,8 +20,28 @@ console.log(interestRate);
 let name = 'JJ'; // String Literal
 let age = 30; // Number Literal
 let isApproved = false; // Boolean Literal
-firstName = undefined;
-let selectedColor = null;
+firstName = undefined; // undefined
+let selectedColor = null; // null
 
 // Static Languages - type of variables are set when declared and cannot be changed in the future
 // Dynamic Languages (JS) - type of variables can change at runtime
+
+// Reference Types : Objects/Arrays/Functions
+// Objects:
+
+let person = {
+  name: 'Itai',
+  age: 20
+};
+console.log(person);
+
+// 2 Ways of retrieving an object property:
+
+// Dot Notation
+person.name = 'Jack';
+console.log(person.name);
+
+// Bracket Notation
+let selection = 'name';
+person[selection] = 'Christian';
+console.log(person[selection]);
