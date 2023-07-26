7c02644fce4dad9e63d2cc591647ef652d840597
JJ PC <hellojjlin@gmail.com>
2023-07-25 20:45:12 -0400

    Variables

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-// This is my first JavaScript code!
-console.log('Hello World');
+
+let name = 'Jack';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// Are case-sensitive
+
+let firstName = 'JJ';
+let lastName = 'Lin';
